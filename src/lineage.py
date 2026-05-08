307a3ab333a5f071221734fdaadd6b23d93ef384
Sanjay G <gokulesh2615@gmail.com>
2026-05-08 19:20:14 +0530

    feat: add src/lineage.py

---
+++ src/lineage.py
@@ -0,0 +1,8 @@
+class Lineage:
+    def __init__(self, model, repository):
+        self.model = model
+        self.repository = repository
+
+    def trace(self):
+        # Implement model lineage tracing logic here
+        pass
